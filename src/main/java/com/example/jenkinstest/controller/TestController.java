9d23bb0fadb083149c31e9b8fe95fa1a17ad4d44
od <od@sweettracker.co.kr>
2024-02-06 16:53:04 +0900

    feat: loki 테스트를 위한 log 컨트롤러 생성

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.jenkinstest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "test22";
+    @GetMapping("/info")
+    public String infoLog() {
+        log.info("info");
+        return "info";
+    }
+
+    @GetMapping("/error")
+    public String errorLog() {
+        log.error("error");
+        return "error";
     }
 }
